8cd4c0e8622fc1e4d0ad8f8dbb26773a441530fb
AminSafari207 <safarin207368@gmail.com>
2025-09-14 20:08:22 +0330

    Implement CheckingAccountRepository interface

---
+++ src/main/java/repository/account/CheckingAccountRepository.java
@@ -0,0 +1,7 @@
+package repository.account;
+
+import model.account.CheckingAccount;
+import repository.base.BaseRepository;
+
+public interface CheckingAccountRepository extends BaseRepository<CheckingAccount, Long> {
+}
