47d4d70eb2f5abb19a88fd6c349c883d9a7d06f6
AminSafari207 <safarin207368@gmail.com>
2025-09-15 21:26:47 +0330

    Implement CustomerRepositoryImpl class

---
+++ src/main/java/repository/customer/CustomerRepositoryImpl.java
@@ -0,0 +1,24 @@
+package repository.customer;
+
+import jakarta.persistence.EntityManager;
+import model.customer.Customer;
+import repository.base.AbstractBaseRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerRepositoryImpl extends AbstractBaseRepository<Customer, Long> implements CustomerRepository {
+    public CustomerRepositoryImpl(EntityManager em) {
+        super(em, Customer.class);
+    }
+
+    @Override
+    public Optional<Customer> findByEmail(String email) {
+        List<Customer> list = em.createQuery("select c from " + getEntityName() + " where c.email = :email", getEntityClassRef())
+                .setParameter("email", email)
+                .setMaxResults(1)
+                .getResultList();
+
+        return list.isEmpty() ? Optional.empty() : Optional.of(list.getFirst());
+    }
+}
