4ff0f586f13365f068f95f6328b99a92a4410747
AminSafari207 <safarin207368@gmail.com>
2025-09-15 10:49:18 +0330

    Implement TransactionalService abstract class

---
+++ src/main/java/service/base/TransactionalService.java
@@ -0,0 +1,56 @@
+package service.base;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public abstract class TransactionalService {
+    protected final EntityManagerFactory emf;
+
+    public TransactionalService(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    public void executeTransactionConsumer(Consumer<EntityManager> consumer) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            consumer.accept(em);
+            tx.commit();
+        } catch (RuntimeException e) {
+            if (tx.isActive()) tx.rollback();
+            throw e;
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw new RuntimeException("Transaction failed.", e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public <R> R executeTransactionFunction(Function<EntityManager, R> function) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            R res = function.apply(em);
+            tx.commit();
+
+            return res;
+        } catch (RuntimeException e) {
+            if (tx.isActive()) tx.rollback();
+            throw e;
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw new RuntimeException("Transaction failed.", e);
+        } finally {
+            em.close();
+        }
+    }
+}
